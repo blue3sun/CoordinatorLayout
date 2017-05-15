d28a19473e6ab211d818e23c9ba83ca11c9ca631
zenglanjing <zenglanjing@bravowhale.com>
2017-05-15 18:09:45 +0800

    Initial commit

---
+++ app/src/main/java/com/monkey/coordinatorlayout/MainActivity.java
@@ -0,0 +1,56 @@
+package com.monkey.coordinatorlayout;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFloatingActinonBar;
+    private CoordinatorLayout mCoordinatorLayout;
+    private CollapsingToolbarLayout mCollapsingToolbarLayout;
+    private Toolbar mToolBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        //setActionBar();
+    }
+
+    private void setActionBar() {
+        setSupportActionBar(mToolBar);
+        android.support.v7.app.ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowTitleEnabled(true);
+        }
+    }
+
+    private void initView() {
+        mCollapsingToolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar_layout);
+        mCoordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinator_layout);
+        mCollapsingToolbarLayout.setTitle("CollapsingToolbarLayout");
+        mToolBar = (Toolbar)findViewById(R.id.toolbar);
+        mFloatingActinonBar = (FloatingActionButton)findViewById(R.id.floating_action_button);
+        mFloatingActinonBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(mCoordinatorLayout,"SnackBar......",Snackbar.LENGTH_LONG)
+                        .setAction("cancel", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "点击了按钮", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+    }
+}
